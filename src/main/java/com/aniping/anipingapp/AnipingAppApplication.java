970c6dd1a2c4d59d02deceb03081c8af97476588
levihisoka <juj2461@gmail.com>
2026-02-11 14:39:45 +0900

    rm develop aws code

--- src/main/java/com/aniping/anipingapp/AnipingAppApplication.java
+++ src/main/java/com/aniping/anipingapp/AnipingAppApplication.java
@@ -16,17 +16,17 @@ public class AnipingAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnipingAppApplication.class, args);
     }
-    // 서버가 뜨자마자 로그에 버킷 리스트가 찍힙니다.
-    @Bean
-    CommandLineRunner test(AwsCheckService awsCheckService) {
-        return args -> {
-            awsCheckService.checkConnection();
-        };
-    }
-    @Bean
-    CommandLineRunner test2(MyOtherService myOtherService) {
-        return args -> {
-            myOtherService.someMethod();
-        };
-    }
+//    // 서버가 뜨자마자 로그에 버킷 리스트가 찍힙니다.
+//    @Bean
+//    CommandLineRunner test(AwsCheckService awsCheckService) {
+//        return args -> {
+//            awsCheckService.checkConnection();
+//        };
+//    }
+//    @Bean
+//    CommandLineRunner test2(MyOtherService myOtherService) {
+//        return args -> {
+//            myOtherService.someMethod();
+//        };
+//    }
 }
